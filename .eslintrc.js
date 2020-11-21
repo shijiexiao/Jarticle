d93ce73da3bde3375c9555bce759bd664055f27f
shijiexiao <shijiexiao@tencent.com>
2020-11-21 18:06:53 +0800

    firt ColumnList

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,25 @@ module.exports = {
   env: {
     node: true
   },
+  parser: 'vue-eslint-parser',
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/standard',
     '@vue/typescript/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    // sourceType: 'module'
+
   },
+  "env": { // eg如果不配置browser，window就会被eslint报undefined的错
+    "es6": true,
+    "browser": true,
+    "node": true
+  },
+
   rules: {
+    "space-before-function-paren": 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <ColumnList :list="list" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'niubi',
+    description: 'djfksdjkfsjd',
+    avatar:
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605958843584&di=3a65072518d4912575af78a2bb40ecee&imgtype=0&src=http%3A%2F%2Fc.hiphotos.baidu.com%2Fexp%2Fw%3D500%2Fsign%3D890b874e0bb30f24359aec03f894d192%2F8718367adab44aeddcf192cbb71c8701a18bfb66.jpg'
+  },
+  {
+    id: 2,
+    title: 'niubi',
+    description: 'djfksdjkfsjd',
+    avatar:
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1605958843584&di=3a65072518d4912575af78a2bb40ecee&imgtype=0&src=http%3A%2F%2Fc.hiphotos.baidu.com%2Fexp%2Fw%3D500%2Fsign%3D890b874e0bb30f24359aec03f894d192%2F8718367adab44aeddcf192cbb71c8701a18bfb66.jpg'
+  }
+]
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    return {
+      list: testData
+    }
+  },
+  components: { ColumnList }
 })
 </script>
 
